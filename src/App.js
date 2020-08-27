67174f536f27af231f0eb6f34754b5f525011aa0
MacBookPro <macbookpro@MacBookPros-MBP.attlocal.net>
2020-08-27 15:01:14 -0500

    first commit / developed the main grid for RGB color blocks.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RGBGridContainer from './containers/RGBGridContainer';
+
+function App() {
+  return (
+    <div className="App">
+        <RGBGridContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/RGBGridContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class RGBGridContainer extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="square"></div>
+                <div className="square"></div>
+                <div className="square"></div>
+                <div className="square"></div>
+                <div className="square"></div>
+                <div className="square"></div>
+            </div>
+        )
+    }
+}
